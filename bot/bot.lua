b5851332168544e7152ee9ec92599b41946717b8
hsamdew <hsamdew@gmail.com>
2017-04-20 11:29:15 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -62,8 +62,7 @@ function whoami()
 	usr = string.gsub(usr, '%s+$', '')
 	usr = string.gsub(usr, '[\n\r]+', ' ') 
 	if usr:match("^root$") then
-		tcpath = '/root/.telegram-cli'
-	elseif not usr:match("^root$") then
+		tcpath = '/root/.telegram-cli'	elseif not usr:match("^root$") then
 		tcpath = '/home/'..usr..'/.telegram-cli'
 	end
   print('>> Download Path = '..tcpath)
@@ -93,29 +92,10 @@ function create_config( )
     info_text = [[》Beyond Reborn V5.0
 An advanced administration bot based on https://valtman.name/telegram-cli
 
-》https://github.com/BeyondTeam/BDReborn
-
+》https://telegram.me/Wriors_Tm
 》Admins :
-》@SoLiD ➣ Founder & Developer《
-》@Makan ➣ Developer & Sponser《
-》@Rixel ➣ Developer 《
-》@Exacute ➣ Developer《
-》@ToOfan ➣ Developer《
-》@CiveY ➣ Developer
-》@K_a_I_i_I_i_n_u_x ➣ Developer《
-》@CliFather ➣ Developer《
-
-》Special thanks to :
-》@Vysheng
-》@MrHalix
-》@Nero_Dev
-》And Beyond Team Members
-
-》Our channel :
-》@BeyondTeam《
-
-》Our website :
-》http://BeyondTeam.ir
+》@hsmdewx3 ➣ Founder & Developer《
+》@Dew_Master ➣ Developer & Sponser《
 ]],
   }
 	serialize_to_file(config, './data/config.lua')
